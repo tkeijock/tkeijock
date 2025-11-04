2931c8c000d70a6451d8153d3c06926ae435053e
tkeijock <tkeijock.work@gmail.com>
2025-11-04 17:56:36 -0300

    fixed indentation and lines inside main and test2

--- Basic Api/test_main.py
+++ Basic Api/test_main.py
@@ -4,9 +4,7 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
-    
